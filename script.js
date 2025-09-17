aefc02481a09a1166d74d8392bfc0f1be38f3e0c
Nugie kurniawan <nugiebinhar@gmail.com>
2025-09-17 15:29:14 +0700

    update/detail film

--- script.js
+++ script.js
@@ -31,11 +31,50 @@ link.addEventListener("input",function(event){
             }else{
                 konten.innerHTML = `<h1 class="font-bold text-xl">${response.Error}</h1>`
             }
-        }).finally(()=>{
-            console.log("Sudah beres")
+          
+            return response
+        })
+        .then(({Search})=>history.pushState({page:"search",keyword:link.value,data:Search},"",`?search=${link.value}`))
+        .finally((response)=>{
+            console.log(response)
+            
+
         })
     },500)
 })
+
+window.addEventListener("popstate",function(event){
+    konten.className = "grid grid-cols-4 gap-4 mt-6"
+    konten.innerHTML = tampilData(event.state.data)
+})
+
+function renderDetail(object){
+    let teks =  `
+                <!-- Gambar -->
+                <div class="w-1/3">
+                    <img
+                        src="${object.Poster}"
+                        alt="Poster Film"
+                        class="w-full h-full object-cover"
+                    />
+                </div>
+
+                <!-- Detail Film -->
+                <div class="w-2/3 p-6">
+                    <h2 class="text-2xl font-bold text-gray-800">${object.Title}</h2>
+                    <p class="text-gray-600 mt-2">
+                        Tahun: ${object.Year}<br />
+                        Genre: ${object.Genre}<br />
+                        Durasi: ${object.runtime}
+                    </p>
+                    <p class="mt-4 text-gray-700 leading-relaxed">
+                        ${object.Plot}
+                    </p>
+                </div>
+           `
+        konten.className = "max-w-3xl mx-auto bg-white shadow-lg rounded-lg overflow-hidden flex"
+        return konten.innerHTML = teks
+}
 //  link.addEventListener("input",function(event){
 //     const ambil =  new Promise((resolve,reject)=>{
 //         const xhr = new XMLHttpRequest()
@@ -75,7 +114,7 @@ function tampilData(object){
                         <p class="text-gray-600 mt-2">
                            ${el.Year}
                         </p>
-                        <button class="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
+                        <button class="button mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600" data-imdbID="${el.imdbID}">
                             Lihat Detail
                         </button>
                     </div>
@@ -86,5 +125,18 @@ function tampilData(object){
 // let movies = getData(`http://www.omdbapi.com/?apikey=bb6e65b5&s=anime`)
 
 
+document.querySelector(".home").addEventListener("click",()=>{
+    window.location.reload()
+})
+
+konten.addEventListener("click",function(e){
+    if(e.target.classList.contains("button")){
+        history.pushState({page:"detail",id:e.target.getAttribute("data-imdbID")},"","?id="+e.target.getAttribute("data-imdbID"))
+        getData(`http://www.omdbapi.com/?apikey=bb6e65b5&i=${e.target.getAttribute("data-imdbID")}`)
+            .then(response=>{
+                renderDetail(response)
+            })
+    }
+})
 
 
